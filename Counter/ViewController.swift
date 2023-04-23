15ee314caa8ce5c181aafe7645523ebeb7df7e3f
Denis <artkamadi@gmail.com>
2023-04-23 23:28:58 +0300

    добавлен Label  и Button  и счетчик на +1

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var clickButton: UIButton!
+    
+    @IBOutlet weak var clickLabel: UILabel!
+    
+   var clicks = 0 {
+        didSet {
+            clickLabel.text = "Значение счётчика: \(clicks)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
 
-
+    @IBAction func clickButtonAction(_ sender: Any) {
+        clicks = clicks + 1
+    }
+    
 }
 
